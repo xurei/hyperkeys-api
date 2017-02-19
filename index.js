b2ccd5ec91c19752345f0778cc4ee7c965decf75
Olivier Bourdoux <olivier.bourdoux@gmail.com>
2017-02-19 18:33:12 +0100

    API jsdoc

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+/**
+ * @type {{
+ *  platform: {
+ *      name: {string},
+ *      arch: {string},
+ *
+ *      isWin: {boolean},
+ *      isWin32: {boolean},
+ *      isWin64: {boolean},
+ *
+ *      isMac: {boolean},
+ *      isLinux: {boolean}
+ *  }
+ * }}
+ */
 module.exports = {
 	platform: require('./platform')
 };
